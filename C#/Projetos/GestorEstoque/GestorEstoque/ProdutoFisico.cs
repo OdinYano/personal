c0a98304873f3269b7eec52a79e7c958db32d9e4
OdinYano <renannfonseca@gmail.com>
2023-06-07 10:28:37 -0300

    gestor de estoque em console

---
+++ C#/Projetos/GestorEstoque/GestorEstoque/ProdutoFisico.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorEstoque
+{
+    [System.Serializable]
+    class ProdutoFisico : Produto, IEstoque
+    {
+        public float frete;
+        private int estoque;
+
+        public ProdutoFisico(string nome, float preco, float frete)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.frete = frete;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine($"Adicionar entrada no estoque do produto {nome}");
+            Console.WriteLine("Digite a quantidade que você quer dar entrada: ");
+            int entrada = int.Parse(Console.ReadLine());
+            estoque += entrada;
+            Console.WriteLine("Entrada registrada!");
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar saída no estoque do produto {nome}");
+            Console.WriteLine("Digite a quantidade que você quer dar saida: ");
+            int saida = int.Parse(Console.ReadLine());
+            estoque -= saida;
+            Console.WriteLine("Saída registrada!");
+
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine("Tipo: Produto fisico");
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Frete: {frete}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Estoque: {estoque}");
+            Console.WriteLine("===========================");
+        }
+    }
+}
